323cdd7bf228deb0320053e08e03f62ba7acd07d
mayakaconard <mayakaconard@gmail.com>
2018-09-10 04:07:25 +0300

    2nd commit

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
